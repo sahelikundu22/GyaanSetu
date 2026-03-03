6282f1224a308fb97e7a5523915c25bb4ebca690
Nayanika Chatterjee <nayanikachatterjee.college@gmail.com>
2026-03-03 20:16:13 +0530

    trials

--- quiz.py
+++ quiz.py
@@ -1,21 +1,24 @@
 import streamlit as st
-from quizdata import QUIZ
+from quiz_data import QUIZ
 
 def start_quiz():
     st.subheader("📝 Motion – Quiz")
 
     score = 0
+    answers = {}
 
     for i, q in enumerate(QUIZ):
         st.write(f"Q{i+1}. {q['question']}")
-        ans = st.radio(
-            "Select answer",
-            q["options"],
-            key=i
+
+        answers[i] = st.radio(
+            label="Choose one:",
+            options=q["options"],
+            key=f"question_{i}"
         )
 
-        if ans == q["answer"]:
-            score += 1
-
     if st.button("Submit Quiz"):
+        for i, q in enumerate(QUIZ):
+            if answers[i] == q["answer"]:
+                score += 1
+
         st.success(f"Your score: {score} / {len(QUIZ)}")
